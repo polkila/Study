187443589b5f0b891a1adfc73a4dcaf77c671455
O <polkila@gmail.com>
2023-10-25 19:55:35 +0300

    Update Lesson10.py

--- 358203/Lesson10.py
+++ 358203/Lesson10.py
@@ -1,12 +1,10 @@
 # https://github.com/polkila/Study/blob/main/358203/Lesson10.py
-import pandas as pd
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-cats = list(set(lst))
-table = {}
-for cat in cats:
-    table[cat] = [1 if i==cat else 0 for i in lst]
-data = pd.DataFrame(table)
-data.head()
+
+def get_doomies(l):
+    return {cat: [1 if i==cat else 0 for i in l] for cat in list(set(l))}
+
+print(get_doomies(lst))
