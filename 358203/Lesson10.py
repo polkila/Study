6f6df2fa7beaac073565558b41365653f47082c1
O <polkila@gmail.com>
2023-10-25 19:46:18 +0300

    Update Lesson10.py

--- 358203/Lesson10.py
+++ 358203/Lesson10.py
@@ -1,12 +1,12 @@
-# 
+# https://github.com/polkila/Study/blob/main/358203/Lesson10.py
 import pandas as pd
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = {
-    'robot': [1 if i=='robot' else 0 for i in lst],
-    'human': [1 if i=='human' else 0 for i in lst],
-}
-data = pd.DataFrame(data)
+cats = list(set(lst))
+table = {}
+for cat in cats:
+    table[cat] = [1 if i==cat else 0 for i in lst]
+data = pd.DataFrame(table)
 data.head()
