030bb2be7cf71ffc1f98427c1a8923dcfbe0bba4
O <polkila@gmail.com>
2023-10-26 06:13:11 +0300

    Update Lesson10.py

--- 358203/Lesson10.py
+++ 358203/Lesson10.py
@@ -1,21 +1,27 @@
 # https://github.com/polkila/Study/blob/main/358203/Lesson10.py
+import pandas as pd
+
+
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 
+
 # 1
 def get_doomies(l):
     return {cat: [1 if i==cat else 0 for i in l] for cat in set(l)}
 
-print(get_doomies(lst))
+data = pd.DataFrame(get_doomies(lst))
+print(data.head())
+
 
 # 2
-import pandas as pd
 data = pd.DataFrame({'whoAmI':lst})
-data['robot'] = 0
-data.loc[data['whoAmI']=='robot', 'robot'] = 1
-data['human'] = 0
-data.loc[data['whoAmI']=='human', 'human'] = 1
-data.drop(columns='whoAmI', inplace=True)
+def to_doomies(dataframe, column_name):
+    for i in set(dataframe[column_name]):
+        dataframe[i] = 0
+        dataframe.loc[dataframe[column_name]==i, i] = 1
+    dataframe.drop(columns=column_name, inplace=True)
+to_doomies(data, 'whoAmI')
 print(data.head())
